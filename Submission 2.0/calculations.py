a7ffb2f913d7dcf65cf4ba8efd1f1970c4713a2d
Sai Siddartha Maram <smaram_be16@thapar.edu>
2019-02-25 23:12:10 +0530

    error offset 100

--- Submission 2.0/calculations.py
+++ Submission 2.0/calculations.py
@@ -8,10 +8,5 @@ fx =  2468.6668434782608
 img=cv2.imread('0002875.jpg')
 Y,X,_=img.shape
 
-print(fx)
-print(X)
-print('height of the image: Y',Y)
-print('width of the image: X',X)
-
-focal_length_in_mm = fx/X
-print(focal_length_in_mm)
+focal_length_in_mm = (fx*15)/X
+print("{}mm".format(focal_length_in_mm))
--- Submission 2.0/check_point3.py
+++ Submission 2.0/check_point3.py
@@ -1,14 +1,16 @@
 import cv2
 import math
 import numpy as np 
+import matplotlib.pyplot as plt
+
 
 
 #reading focal lenghths of the image
 fx=2468.6668434782608
 
 #reading image
-img_before_distance = cv2.imread('0002875.jpg')
-img_after_distance = cv2.imread('0002876.jpg')
+img_before_distance = cv2.imread('0000745.jpg')
+img_after_distance = cv2.imread('0000746.jpg')
 
 
 #distortion matrics
@@ -40,19 +42,31 @@ img_after_distance = img_after_distance[y:y+h, x:x+w]
 image_center = (image_width/2,image_height/2)
 
 #location of signs in image
-location_sign_before_distance=((591+591+42)/2,(102+102+69)/2)
-location_sign_after_distance=((531+531+51)/2,(108+108+69)/2)
+
+
+sign_1_top_left_x=1707
+sign_1_top_left_y=105
+sign_1_width=69
+sign_1_height=75
+
+sign_2_top_left_x=1740
+sign_2_top_left_y=111
+sign_2_width=69
+sign_2_height=78
+
+location_sign_before_distance=((sign_1_top_left_x+sign_1_top_left_x+sign_1_width)/2,(sign_1_top_left_y+sign_1_top_left_y+sign_1_height)/2)
+location_sign_after_distance=((sign_2_top_left_x+sign_2_top_left_x+sign_2_width)/2,(sign_2_top_left_y+sign_2_top_left_y+sign_2_height)/2)
 
 #display images
-cv2.rectangle(img_before_distance,(591,102),(591+42,102+69),(0,0,0),1)
-cv2.rectangle(img_after_distance,(531,108),(531+51,108+69),(0,0,0),1)
+cv2.rectangle(img_before_distance,(1707,105),(1707+69,105+75),(0,0,0),1)
+cv2.rectangle(img_after_distance,(1740,111),(1740+69,111+78),(0,0,0),1)
 
-#mark centers of signs
+# #mark centers of signs
 cv2.circle(img_before_distance,location_sign_before_distance,3,(255,0,0),4)
 cv2.circle(img_after_distance,location_sign_after_distance,3,(255,0,0),4)
 cv2.circle(img_after_distance,image_center,3,(255,0,0),4,-1)
 
-#image center to sign center
+# #image center to sign center
 cv2.line(img_before_distance,image_center,location_sign_before_distance,(0,0,0),5,-1)
 cv2.line(img_after_distance,image_center,location_sign_after_distance,(0,0,0),5,-1)
 
@@ -64,10 +78,12 @@ cv2.line(img_before_distance,(0,image_height/2),(image_width,image_height/2),(0,
 cv2.line(img_after_distance,(0,image_height/2),(image_width,image_height/2),(0,0,0),4,-1)
 
 #display images
+img_before_distance=cv2.resize(img_before_distance,(500,500))
+img_after_distance=cv2.resize(img_after_distance,(500,500))
 cv2.imshow('image_before_distance', img_before_distance)
 cv2.imshow('image_after_distance', img_after_distance)
 
-
+cv2.waitKey(0)
 #destroy all windoes
 cv2.destroyAllWindows()
 
@@ -89,14 +105,15 @@ print(l)
 print(w)
 
 #comparision
-g_truth=(736465.73748737,3750769.21175686)
-camera_x_cordinate=736516.45814926
-camera_y_cordinate= 3750734.29288626
-camera=(camera_x_cordinate,camera_y_cordinate)
-pred_1=(camera[0]-w,camera[1]+l)
+g_truth=(732063.6161539537,3738027.0026369933)
+camera=(732095.4582989508,3738103.923706733)
+pred_1=(camera[0]+w,camera[1]+l)
 
 
 #compare results
 print("actual:",g_truth)
 print("pred1:",pred_1)
+print("x cordinate missing mark by: ",g_truth[0]-pred_1[0])
+print("y_cordinate missing mark by:", g_truth[1]-pred_1[1])
+
 
