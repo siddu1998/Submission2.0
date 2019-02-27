5db51e3cabbc82347c522b552e53f4ff4629773b
Sai Siddartha Maram <smaram_be16@thapar.edu>
2019-02-27 18:13:51 +0530

    roi elimination and point cordinates

--- Submission 2.0/mouse_application.py
+++ Submission 2.0/mouse_application.py
@@ -1,6 +1,16 @@
 import cv2
 import numpy as numpy
 
+
+
 point1=(0,0)
 point2=(0,0)
+
+
+
+def mouse_callback_get_cordinates(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(param,(x,y),5,(255,0,0),-1)
+        print("x:{},y:{}".format(x,y))
+
 
