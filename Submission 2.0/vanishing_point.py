43dfb29f0055a70df29e42b319d367cc528599ae
Sai Siddartha Maram <smaram_be16@thapar.edu>
2019-03-20 23:02:13 +0530

    perspective transform into bird for potoles

--- Submission 2.0/vanishing_point.py
+++ Submission 2.0/vanishing_point.py
@@ -1,39 +1,80 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+
+from birdseye import BirdsEye
+
 import numpy as np
 import cv2
-import math
-
-
 def region_of_interest(img, vertices):
+    # Define a blank matrix that matches the image height/width.
     mask = np.zeros_like(img)
-    match_mask_color = 255 # <-- This line altered for grayscale.
-    
+    # Retrieve the number of color channels of the image.
+    channel_count = img.shape[2]
+    # Create a match color with the same color channel counts.
+    match_mask_color = (255,) * channel_count
+      
+    # Fill inside the polygon
     cv2.fillPoly(mask, vertices, match_mask_color)
+    
+    # Returning the image only where mask pixels match
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
 
-image = mpimg.imread('0002879.jpg')
-height,width,_=image.shape
-region_of_interest_vertices = [
-    (0, height),
-    (width / 2, height / 2),
-    (width, height),
-]
+
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+image = mpimg.imread('0017781.jpg')
 plt.figure()
 plt.imshow(image)
 plt.show()
-gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-cannyed_image = cv2.Canny(gray_image, 100, 200)
-# Moved the cropping operation to the end of the pipeline.
+height,width,_=image.shape
+region_of_interest_vertices = [
+    (0, height),
+    (659,715),
+    (753,709),
+    (width, height),
+]
+
 cropped_image = region_of_interest(
-    cannyed_image,
-    np.array([region_of_interest_vertices], np.int32)
+    image,
+    np.array([region_of_interest_vertices], np.int32),
 )
+# cv2.line(image,(0,height),(int(width/2),int(height/2)),(255,255,255),3)
+# cv2.line(image,(width,height),(int(width/2),int(height/2)),(255,255,255),3)
+
+gray_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+
+cannyed_image = cv2.Canny(gray_image,100,130)
+
+
 plt.figure()
+plt.subplot(2,2,1)
 plt.imshow(cropped_image)
-cv2.imshow('crooped_canny',cropped_image)
-cv2.imwrite('sdf.jpg',cropped_image)
-cv2.waitKey(0)
+
+plt.subplot(2,2,2)
+plt.imshow(image)
+
+plt.subplot(2,2,3)
+plt.imshow(gray_image)
+
+
+plt.subplot(2,2,4)
+plt.imshow(cannyed_image)
 plt.show()
+#bl,tl,tr,br
+#source_points=np.float32([ [0, height],[659,715],[753,709],[width, height]  ])
+source_points=np.float32([ [620, 780],[637,734],[789,728],[813, 766]  ])
+#destination_points = np.float32([ [0, 600], [0, 0], [600, 0], [600, 600] ])
+destination_points = np.float32([ [0, 2048], [0, 0], [2248, 0], [2248, 2048] ])
+print(type(destination_points))
+
+matrix = cv2.getPerspectiveTransform(source_points, destination_points)
+result = cv2.warpPerspective(cropped_image, matrix, (2248, 2048))
+
+
+plt.figure()
+plt.imshow(result)
+plt.show()
+
