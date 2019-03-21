da78971927abfb29e7c52ef5d714a79837cff830
Sai Siddartha Maram <smaram_be16@thapar.edu>
2019-03-21 15:29:51 +0530

    error log i75

--- Submission 2.0/research_paper_implmentation.py
+++ Submission 2.0/research_paper_implmentation.py
@@ -5,20 +5,22 @@ from math import cos
 image_file_name='0002879.jpg'
 image = cv2.imread(image_file_name)
 image_height,image_width,_=image.shape
-vanishing_point = (image_height/2, image_height/2)
+vanishing_point = (int(image_width/2), int(image_height/2))
 
-Y = abs(332-vanishing_point[1]) #pixels
-print(Y)
-H=1.85 #meters
-f = 2468.6668434782608
-Z = (H*f)/Y
-X = abs(339-vanishing_point[0])
+cv2.circle(image,vanishing_point,4,(0,0,0))
+cv2.circle(image,(318,138),4,(0,0,0))
+
+Y = abs(138-vanishing_point[1]) #pixels
+
+H=2 #meters
+f = 2468.6668434782608 #pixels
+Z = (H*f)/Y #m *pixel/pixel == meters
+X = abs(318-vanishing_point[0])
 
 angle_beta = X/Z
 print("inpath_distance:",Z)
 print("cosine distance:", Z/cos(angle_beta))
-
-
-
-
+camera_location=parsing_camrea_annotations(image_file_name,'i75_camera_cordinates.csv')
+print(camera_location[1])
+print(camera_location[1]+Z)
 
